e04a5d833ad5ca754d3a1ec3dfacb89957b777b1
Avishek Behera <avishekbehera87@gmail.com>
2021-06-14 18:49:27 +0530

    JIRA-1122: Added Bye endpoint

---
+++ src/main/java/com/sdetlearning/qa/Bye.java
@@ -0,0 +1,21 @@
+package com.sdetlearning.qa;
+
+public class Bye {
+
+        private final long id;
+        private final String content;
+
+        public Bye(long id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+    }
--- src/main/java/com/sdetlearning/qa/GreetingController.java
+++ src/main/java/com/sdetlearning/qa/GreetingController.java
@@ -1,4 +1,5 @@
 package com.sdetlearning.qa;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,20 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-        private static final String template = "Hello, %s!";
-        private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+    private static final String template2 = "Bye, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-        @GetMapping("/greeting")
-        public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-            return new Greeting(counter.incrementAndGet(), String.format(template, name));
-        }
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "Fname", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/bye")
+    public Bye bye(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Bye(counter.incrementAndGet(), String.format(template2, name));
+    }
+
+
+}
 
